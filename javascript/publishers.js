6bd0bd5590485e226d395b554b708a2872585652
antoniojosehc <antoniojosehc@hotmail.com>
2024-05-16 22:12:03 -0500

    Agregué la función loadData para llamar la info del servidor

--- javascript/publishers.js
+++ javascript/publishers.js
@@ -201,3 +201,18 @@ function eliminar(i) {
         printTable(arrTemp);
     }
 }
+
+function loadData() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8080/publicador");
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            const data = xhr.response;
+            console.log(data);
+        } else {
+            console.log('Error: ${xhr.status}');
+        }
+    };
+}
